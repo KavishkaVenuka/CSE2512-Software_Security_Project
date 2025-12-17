6f7cc6351e09bcd1c607089f83e10b8ddd54e379
kavishka-venuka <kavishkavenuka554pro@gmail.com>
2025-12-18 00:10:19 +0530

    Add all the basic changes

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
